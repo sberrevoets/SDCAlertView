274f227c304f95775fb2d19d36285adf4803ea50
Scott90 <my@fake.email>
2013-09-20 15:29:31 -0500

    Initial Commit

---
+++ SDCAlertView/SDCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SDCViewController.m
+//  SDCAlertView
+//
+//  Created by Scott Berrevoets on 9/20/13.
+//  Copyright (c) 2013 Scotty Doesn't Code. All rights reserved.
+//
+
+#import "SDCViewController.h"
+
+@interface SDCViewController ()
+
+@end
+
+@implementation SDCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SDCAlertViewTests/SDCAlertViewTests.m
@@ -0,0 +1,34 @@
+//
+//  SDCAlertViewTests.m
+//  SDCAlertViewTests
+//
+//  Created by Scott Berrevoets on 9/20/13.
+//  Copyright (c) 2013 Scotty Doesn't Code. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SDCAlertViewTests : XCTestCase
+
+@end
+
+@implementation SDCAlertViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
