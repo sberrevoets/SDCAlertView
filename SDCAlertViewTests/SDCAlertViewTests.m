ec366b25deeb6579743c01ebefdcf20cf187a7b7
Luke Stringer <lukestringer630@gmail.com>
2013-12-01 16:31:09 +0000

    Test for calling a block as well delegate methods.

--- SDCAlertViewTests/SDCAlertViewTests.m
+++ SDCAlertViewTests/SDCAlertViewTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "SDCAlertView.h"
 
 @interface SDCAlertViewTests : XCTestCase
 
@@ -14,21 +15,31 @@
 
 @implementation SDCAlertViewTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testCallsShouldDismissBlockWithoutDelegate {
+    SDCAlertView *sut = [[SDCAlertView alloc]
+                         initWithTitle:@"Title"
+                         message:@"Message Here"
+                         delegate:nil
+                         cancelButtonTitle:@"Cancel"
+                         otherButtonTitles:nil];
+    
+    __block NSInteger capturedButtonIndex;
+    __block BOOL blockWasCalled = NO;
+    
+    sut.shouldDissmissBlock = ^BOOL (NSInteger buttonIndex) {
+        blockWasCalled = YES;
+        capturedButtonIndex = buttonIndex;
+        return NO;
+    };
+    
+    [sut tappedButtonAtIndex:2];
+    
+    XCTAssertTrue(blockWasCalled, @"");
+    XCTAssertEqual(capturedButtonIndex, 2, @"");
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testCallsShouldDismissBlockAndDelegate {
+    
 }
 
 @end
