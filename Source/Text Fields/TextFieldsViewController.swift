1824503b65e44e12212dd5fce607b136fa841ae4
Scott Berrevoets <s.berrevoets@me.com>
2015-11-22 21:49:53 -0800

    Fix crash on iOS 8 when using text fields
    This crash was presented by Cocoa Touch:
    http://stackoverflow.com/a/30719434/751268

--- Source/Text Fields/TextFieldsViewController.swift
+++ Source/Text Fields/TextFieldsViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 private let kTextFieldCellIdentifier = "textFieldCell"
 
-class TextFieldsViewController: UITableViewController {
+final class TextFieldsViewController: UIViewController {
 
     var requiredHeight: CGFloat {
         return self.tableView.rowHeight * CGFloat(self.tableView.numberOfRowsInSection(0))
@@ -15,33 +15,39 @@ class TextFieldsViewController: UITableViewController {
         }
     }
 
+    private let tableView = UITableView(frame: .zero, style: .Plain)
     private let textFields: [UITextField]
 
     init(textFields: [UITextField]) {
         self.textFields = textFields
-        super.init(style: .Plain)
+        super.init(nibName: nil, bundle: nil)
+    }
 
+    required init?(coder aDecoder: NSCoder) {
+        self.textFields = []
+        super.init(nibName: nil, bundle: nil)
+        return nil
+    }
+
+    override func loadView() {
         let nibName = NSStringFromClass(TextFieldCell).componentsSeparatedByString(".").last!
         let cellNib = UINib(nibName: nibName, bundle: NSBundle(forClass: self.dynamicType))
         self.tableView.registerNib(cellNib, forCellReuseIdentifier: kTextFieldCellIdentifier)
         self.tableView.dataSource = self
         self.tableView.separatorStyle = .None
         self.tableView.scrollEnabled = false
-    }
 
-    required convenience init?(coder aDecoder: NSCoder) {
-        self.init(textFields: [UITextField()])
-        return nil
+        self.view = tableView
     }
 }
 
-extension TextFieldsViewController/*: UITableViewDataSource */ {
+extension TextFieldsViewController: UITableViewDataSource {
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.textFields.count
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
         -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCellWithIdentifier(kTextFieldCellIdentifier,
