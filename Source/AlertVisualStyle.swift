f0b136425203b56cf798e4100e25625a92e874e5
Scott Berrevoets <sberrevoets@lyft.com>
2020-04-30 09:18:09 -0700

    Update Source/AlertVisualStyle.swift

--- Source/AlertVisualStyle.swift
+++ Source/AlertVisualStyle.swift
@@ -98,7 +98,7 @@ open class AlertVisualStyle: NSObject {
     @objc
     public var normalTextColor: UIColor?
 
-The color for a preferred action's text. If `nil`, `normalTextColor` is used.
+    /// The color for a preferred action's text. If `nil`, `normalTextColor` is used.
     @objc
     public var preferredTextColor: UIColor?
 
